26038e99023d087bda6ed3ba520724d15866f40e
Robert Jakobs <robert.jakobs@sap.com>
2020-02-18 16:00:56 +0100

    LeonardoIoT tests: Refactored function declarations and returns

--- test/unit/LeonardoIoT.test.js
+++ test/unit/LeonardoIoT.test.js
@@ -9,26 +9,26 @@ const packageJson = require('../../package.json');
 const forwardedAccessToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiY2lkIjoieHN1YWFDbGllbnRJZCIsInppZCI6ImFkZTU4NmM2LWY1YjEtNGRkYy1hZWNiLWVhZDNjMmU2ZTcyNSIsInNjb3BlIjpbInVhYS51c2VyIl0sIm5hbWUiOiJTQVAgTGVvbmFyZG8gVGVzdDIiLCJpYXQiOjE1MTYyMzkwMjJ9.h-ETrxkX_K2puXZustO8vTD000OvA_HUfcYWOtokMI_trznDhYgFh9uACd2tPPbKbsrKKH-bcQljSH5Nh-l1KsrsMdVKygJ5-Dmv_8Jdqjb7IHsTZSyQ2b0-EcPawcnECd17N9OJJAIVhBDKnW_32eGLv1yd71Z1e4BjIvuUJCClUWO6mFHD7qL4fcL9zb20N25AcEddDbkTgIe0iSWBaO6k1XUne3jcPcgAOGqpH7J04ACxk7366-4wmtDVk00AXOn8MbNeAkI_cOI3nV3V5dxsB6E6eRFJENKX186DNhwHaBzq6h2VKvXKOG_D2THzPRjDMhiEtrAmSQdB15Re_Q';
 const generatedAccessToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiY2lkIjoieHN1YWFDbGllbnRJZCIsInppZCI6ImFkZTU4NmM2LWY1YjEtNGRkYy1hZWNiLWVhZDNjMmU2ZTcyNSIsInNjb3BlIjpbInVhYS51c2VyIl0sIm5hbWUiOiJTQVAgTGVvbmFyZG8gVGVzdDEiLCJpYXQiOjE1MTYyMzkwMjJ9.vnel93jRc4Xg43Kl0t8ysmo4VA2SJ0LdSO7HzCDWAwpy91fS0YcaHkFNFKTNqzEXvveaOOPplY7ULaPy2gAQLIMg_sOKrJGaGjQ3oaxTiq5NyHVxBTP7n8DEvt3kmZv8v6GvIOF4vKl4aHU0pZUGlBFNiBRCAr38sSW2A6AMd2X4Ws7bphFCzlMODPayrdolGEvz866sLa8uK4386SYBrjBRQ9K290icPMNyO0fKNS0qNENjRgazAbgAm7PDNO6j7PUhJgN2apKxsQelrysIrRLIfBDTD7TJsOPzgKU3T8FyOOw1XJBonFMSQ9yCHMFKO3KrL1BuzDdtNZMdxvNFfg';
 
-describe('LeonardoIoT', () => {
+describe('LeonardoIoT', function () {
     let client;
     let tmpVcapServices;
 
-    beforeEach(() => {
+    beforeEach(function () {
         client = new LeonardoIoT();
         tmpVcapServices = JSON.parse(JSON.stringify(process.env.VCAP_SERVICES));
     });
 
-    afterEach(() => {
+    afterEach(function () {
         process.env.VCAP_SERVICES = JSON.parse(JSON.stringify(tmpVcapServices));
     });
 
-    describe('constructor', () => {
-        it('call without arguments', async () => {
+    describe('constructor', function () {
+        it('call without arguments', function () {
             const clientDefault = new LeonardoIoT();
             assert.notEqual(clientDefault, undefined, 'Invalid constructor for LeonardoIoT client');
         });
 
-        it('call with configuration arguments', async () => {
+        it('call with configuration arguments', function () {
             const client = new LeonardoIoT({
                 uaa: {
                     clientid: "testId",
@@ -43,7 +43,7 @@ describe('LeonardoIoT', () => {
             assert.notEqual(client, undefined, 'Invalid constructor for LeonardoIoT client');
         });
 
-        it('call with configuration arguments including xsuaa config', async () => {
+        it('call with configuration arguments including xsuaa config', function () {
             const client = new LeonardoIoT({
                 uaa: {
                     clientid: "testId",
@@ -58,7 +58,7 @@ describe('LeonardoIoT', () => {
             assert.notEqual(client, undefined, 'Invalid constructor for LeonardoIoT client');
         });
 
-        it('instances for multi tenant mode', async () => {
+        it('instances for multi tenant mode', function () {
             const clientTest = new LeonardoIoT('leonardo-iot-account-test');
             const clientDev = new LeonardoIoT('leonardo-iot-account-dev');
 
@@ -68,11 +68,11 @@ describe('LeonardoIoT', () => {
         });
     });
 
-    describe('Request', () => {
-        it('has default parameters', async () => {
+    describe('Request', function () {
+        it('has default parameters', function () {
             let headers = LeonardoIoT._addUserAgent({});
             headers.Authorization = `Bearer ${forwardedAccessToken}`;
-            rpStub = function (requestConfig) {
+            rpStub = (requestConfig) => {
                 AssertionUtil.assertRequestConfig(requestConfig, {
                     url: 'https://appiot-mds.cfapps.eu10.hana.ondemand.com/Things',
                     method: 'GET',
@@ -81,7 +81,6 @@ describe('LeonardoIoT', () => {
                     agentOptions: {},
                     resolveWithFullResponse: false
                 });
-                return Promise.resolve();
             };
             client.authenticator.exchangeToken = async function () {
                 return forwardedAccessToken;
@@ -93,12 +92,12 @@ describe('LeonardoIoT', () => {
             });
         });
 
-        it('has correct version in user agent header field', async () => {
+        it('has correct version in user agent header field', function () {
             let headers = LeonardoIoT._addUserAgent({});
             assert.equal(headers['User-Agent'], `${packageJson.name}-nodejs / ${packageJson.version}`, 'Unexpected User-Agent header field value');
         });
 
-        it('throws error for missing URL parameter', async () => {
+        it('throws error for missing URL parameter', async function() {
             try {
                 await LeonardoIoT._request({url: null});
                 assert.fail('Expected Error was not thrown');
@@ -108,12 +107,11 @@ describe('LeonardoIoT', () => {
         });
     });
 
-    describe('JWT token', () => {
-        it('gets forwarded correctly', async () => {
-            rpStub = function (requestConfig) {
+    describe('JWT token', function () {
+        it('gets forwarded correctly', function () {
+            rpStub = (requestConfig) => {
                 const expectedJwt = `Bearer ${forwardedAccessToken}`;
                 assert.equal(requestConfig.headers.Authorization, expectedJwt, 'Unexpected JWT token forwarding');
-                return Promise.resolve();
             };
             client.authenticator.exchangeToken = async function () {
                 return forwardedAccessToken;
@@ -125,11 +123,10 @@ describe('LeonardoIoT', () => {
             });
         });
 
-        it('gets sliced and forwarded correctly', async () => {
-            rpStub = function (requestConfig) {
+        it('gets sliced and forwarded correctly', function () {
+            rpStub = (requestConfig) => {
                 const expectedJwt = `Bearer ${forwardedAccessToken}`;
                 assert.equal(requestConfig.headers.Authorization, expectedJwt, 'Unexpected JWT token forwarding');
-                return Promise.resolve();
             };
             client.authenticator.exchangeToken = async function () {
                 return forwardedAccessToken;
@@ -141,11 +138,10 @@ describe('LeonardoIoT', () => {
             });
         });
 
-        it('gets fetched from authentication URL for request', async () => {
-            rpStub = function (requestConfig) {
+        it('gets fetched from authentication URL for request', function () {
+            rpStub = (requestConfig) => {
                 const expectedJwt = `Bearer ${generatedAccessToken}`;
                 assert.equal(requestConfig.headers.Authorization, expectedJwt, 'Unexpected JWT token forwarding');
-                return Promise.resolve();
             };
             client.authenticator.getAccessToken = async function () {
                 return generatedAccessToken;
