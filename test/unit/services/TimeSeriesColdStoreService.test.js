e9a38770ffb3a59d38407d84ca256ab88bfe1f1d
Robert Jakobs <robert.jakobs@sap.com>
2020-02-18 15:42:54 +0100

    TimeSeriesColdStoreService tests: Refactored function declarations and returns

--- test/unit/services/TimeSeriesColdStoreService.test.js
+++ test/unit/services/TimeSeriesColdStoreService.test.js
@@ -3,59 +3,59 @@ const LeonardoIoT = require('../../../lib/LeonardoIoT');
 
 const appiotColdstoreUrl = 'https://appiot-coldstore.cfapps.eu10.hana.ondemand.com';
 
-describe('Time Series Cold Store Service', () => {
+describe('Time Series Cold Store Service', function () {
     let client;
 
-    beforeEach(() => {
+    beforeEach(function () {
         client = new LeonardoIoT();
     });
 
-    describe('Time Series Data', () => {
-        it('create', async () => {
+    describe('Time Series Data', function () {
+        it('create', function () {
             const thingId = 'MyThing';
             const thingTypeName = 'MyThingType';
             const propertySetId = 'MyPropertySet';
             const timeSeriesPayload = {value: [{Temperature: '25', _time: '2019-01-15T10:00:00Z'}]};
 
-            client.request = function (requestConfig) {
+            client.request = (requestConfig) => {
                 AssertionUtil.assertRequestConfig(requestConfig, {
                     url: `${appiotColdstoreUrl}/Things('${thingId}')/${thingTypeName}/${propertySetId}`,
                     method: 'PUT',
                     body: timeSeriesPayload
                 });
             };
-            await client.createColdStoreTimeSeriesData(thingId, thingTypeName, propertySetId, timeSeriesPayload);
+            return client.createColdStoreTimeSeriesData(thingId, thingTypeName, propertySetId, timeSeriesPayload);
         });
 
-        it('read', async () => {
+        it('read', function () {
             const thingId = 'MyThing';
             const thingTypeName = 'MyThingType';
             const propertySetId = 'MyPropertySet';
             const fromTime = '2019-01-15T08:00:00Z';
             const toTime = '2019-01-15T20:00:00Z';
-            client.request = function (requestConfig) {
+            client.request = (requestConfig) => {
                 AssertionUtil.assertRequestConfig(requestConfig, {
                     url: `${appiotColdstoreUrl}/Things('${thingId}')/${thingTypeName}/${propertySetId}?timerange=${fromTime}-${toTime}`,
                 });
             };
 
-            await client.getColdStoreTimeSeriesData(thingId, thingTypeName, propertySetId, fromTime, toTime);
+            return client.getColdStoreTimeSeriesData(thingId, thingTypeName, propertySetId, fromTime, toTime);
         });
 
-        it('delete', async () => {
+        it('delete', function () {
             const thingId = 'MyThing';
             const thingTypeName = 'MyThingType';
             const propertySetId = 'MyPropertySet';
             const fromTime = '2019-01-15T08:00:00Z';
             const toTime = '2019-01-15T20:00:00Z';
-            client.request = function (requestConfig) {
+            client.request = (requestConfig) => {
                 AssertionUtil.assertRequestConfig(requestConfig, {
                     url: `${appiotColdstoreUrl}/Things('${thingId}')/${thingTypeName}/${propertySetId}?timerange=${fromTime}-${toTime}`,
                     method: 'DELETE'
                 });
             };
 
-            await client.deleteColdStoreTimeSeriesData(thingId, thingTypeName, propertySetId, fromTime, toTime);
+            return client.deleteColdStoreTimeSeriesData(thingId, thingTypeName, propertySetId, fromTime, toTime);
         });
     });
 });
