836c024fc38e0d17699f57dab5c87e0851f2320d
Robert Jakobs <robert.jakobs@sap.com>
2020-02-19 16:40:15 +0100

    Integration step2: Added error handling

--- test/integration/step2_readAll.js
+++ test/integration/step2_readAll.js
@@ -14,37 +14,52 @@ describe('2) READ ALL', function () {
     });
 
     it('property set types', async function () {
-        try{
-
-        }catch(error){
+        try {
+            await client.getPropertySetTypes();
+        } catch (error) {
             assert.fail(error);
         }
-        await client.getPropertySetTypes();
         return client.getPropertySetTypesByPackage(DataHelper.package().Name);
     });
 
     it('thing types', async function () {
-        await client.getThingTypes();
+        try {
+            await client.getThingTypes();
+        } catch (error) {
+            assert.fail(error);
+        }
         return client.getThingTypesByPackage(DataHelper.package().Name);
     });
 
     it('object groups', async function () {
-        await client.getObjectGroups();
-        const objectGroups = await client.getObjectGroups({$filter: `name eq ${DataHelper.objectGroup().name}`});
-        DataHelper.data.objectGroup = objectGroups.value[0];
+        try {
+            await client.getObjectGroups();
+            const objectGroups = await client.getObjectGroups({$filter: `name eq ${DataHelper.objectGroup().name}`});
+            DataHelper.data.objectGroup = objectGroups.value[0];
+        } catch (error) {
+            assert.fail(error);
+        }
     });
 
     it('things', async function () {
-        await client.getThings();
-        await client.getThingsByThingType(DataHelper.thingType().Name, {$filter: `_name ne 'xyz'`});
-        const things = await client.getThingsByThingType(DataHelper.thingType().Name);
-        DataHelper.data.thing = things.value[0];
+        try {
+            await client.getThings();
+            await client.getThingsByThingType(DataHelper.thingType().Name, {$filter: `_name ne 'xyz'`});
+            const things = await client.getThingsByThingType(DataHelper.thingType().Name);
+            DataHelper.data.thing = things.value[0];
+        } catch (error) {
+            assert.fail(error);
+        }
     });
 
     it('events', async function () {
-        await client.getEvents();
-        await client.getEventsByThingId(DataHelper.data.thing._id, {$filter: `_status ne 'InProcess'`});
-        const events = await client.getEventsByThingId(DataHelper.data.thing._id);
-        DataHelper.data.event = events.value[0];
+        try {
+            await client.getEvents();
+            await client.getEventsByThingId(DataHelper.data.thing._id, {$filter: `_status ne 'InProcess'`});
+            const events = await client.getEventsByThingId(DataHelper.data.thing._id);
+            DataHelper.data.event = events.value[0]; 
+        } catch (error) {
+            assert.fail(error);
+        }
     });
 });
