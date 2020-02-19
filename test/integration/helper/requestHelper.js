b46f10caee78c37f7d966c398feef1e62158b449
Robert Jakobs <robert.jakobs@sap.com>
2020-02-19 17:20:46 +0100

    Integration requestHelper: Added error handling

--- test/integration/helper/requestHelper.js
+++ test/integration/helper/requestHelper.js
@@ -1,39 +1,51 @@
-async function determineTenantPrefix(client) {
-    const tenantInfo = await client.request({
+function determineTenantPrefix(client) {
+    return client.request({
         url: `${client.navigator.businessPartner()}/Tenants`
+    }).then((response) => {
+        return response.value[0].package;
     });
-
-    return tenantInfo.value[0].package;
 }
 
 async function deletePackageCascading(client, packageName) {
-    const thingTypesResponse = await client.getThingTypesByPackage(packageName);
-    const thingTypes = thingTypesResponse.d.results;
-    for (const thingType of thingTypes) {
-        const thingTypeResponse = await client.getThingType(thingType.Name, {}, {resolveWithFullResponse: true});
-        const thingsResponse = await client.getThingsByThingType(thingType.Name);
-        const things = thingsResponse.value;
-        for (const thing of things) {
-            const events = await client.getEventsByThingId(thing._id);
-            for (const event of events.value) {
-                await client.deleteEvent(event._id);
-            }
-            await client.deleteThing(thing._id);
+    let packageResponse;
+    try {
+        const thingTypesResponse = await client.getThingTypesByPackage(packageName);
+        const thingTypes = thingTypesResponse.d.results;
+        for (const thingType of thingTypes) {
+            const thingTypeResponse = await client.getThingType(thingType.Name, {}, {resolveWithFullResponse: true});
+            const thingsResponse = await client.getThingsByThingType(thingType.Name);
+            const thingDeletePromises = thingsResponse.value.map((thing)=>{
+                return new Promise(async (resolve, reject)=>{
+                    try {
+                        const events = await client.getEventsByThingId(thing._id);
+                        const eventDeletePromises = [];
+                        for (const event of events.value) {
+                            eventDeletePromises.push(client.deleteEvent(event._id));
+                        }
+                        await Promise.all(eventDeletePromises);
+                        await client.deleteThing(thing._id);
+                        resolve();
+                    } catch (error) {
+                        reject(error);
+                    } 
+                });
+            });
+            await Promise.all(thingDeletePromises)
+            await client.deleteThingType(thingType.Name, thingTypeResponse.headers.etag);
         }
 
-        await client.deleteThingType(thingType.Name, thingTypeResponse.headers.etag);
+        const pstsResponse = await client.getPropertySetTypesByPackage(packageName);
+        const psts = pstsResponse.d.results;
+        psts.sort((a, b) => ((a.DataCategory === 'ReferencePropertyData') ? -1 : ((b.DataCategory === 'ReferencePropertyData') ? 1 : 0)));
+        for (const pst of psts) {
+            const pstResponse = await client.getPropertySetType(pst.Name, {}, {resolveWithFullResponse: true});
+            await client.deletePropertySetType(pst.Name, pstResponse.headers.etag);
+        }
+        packageResponse = await client.getPackage(packageName, {resolveWithFullResponse: true});
+    }catch (error) {
+        throw (error);
     }
-
-    const pstsResponse = await client.getPropertySetTypesByPackage(packageName);
-    const psts = pstsResponse.d.results;
-    psts.sort((a, b) => ((a.DataCategory === 'ReferencePropertyData') ? -1 : ((b.DataCategory === 'ReferencePropertyData') ? 1 : 0)));
-    for (const pst of psts) {
-        const pstResponse = await client.getPropertySetType(pst.Name, {}, {resolveWithFullResponse: true});
-        await client.deletePropertySetType(pst.Name, pstResponse.headers.etag);
-    }
-
-    const packageResponse = await client.getPackage(packageName, {resolveWithFullResponse: true});
-    await client.deletePackage(packageName, packageResponse.headers.etag);
+    return client.deletePackage(packageName, packageResponse.headers.etag);
 }
 
 module.exports = {
