26b90680d51a472b681b342137fe575ae505c1a3
Robert Jakobs <robert.jakobs@sap.com>
2020-02-20 16:03:37 +0100

    Integration requestHelper: Fix for several eslint errors

--- test/integration/helper/requestHelper.js
+++ test/integration/helper/requestHelper.js
@@ -1,9 +1,7 @@
 function determineTenantPrefix(client) {
     return client.request({
         url: `${client.navigator.businessPartner()}/Tenants`
-    }).then((response) => {
-        return response.value[0].package;
-    });
+    }).then((response) => response.value[0].package);
 }
 
 async function deletePackageCascading(client, packageName) {
@@ -12,25 +10,20 @@ async function deletePackageCascading(client, packageName) {
         const thingTypesResponse = await client.getThingTypesByPackage(packageName);
         const thingTypes = thingTypesResponse.d.results;
         for (const thingType of thingTypes) {
-            const thingTypeResponse = await client.getThingType(thingType.Name, {}, {resolveWithFullResponse: true});
+            const thingTypeResponse = await client.getThingType(thingType.Name, {}, { resolveWithFullResponse: true });
             const thingsResponse = await client.getThingsByThingType(thingType.Name);
-            const thingDeletePromises = thingsResponse.value.map((thing)=>{
-                return new Promise(async (resolve, reject)=>{
+            const thingDeletePromises = thingsResponse.value.map((thing) => new Promise(async (resolve, reject) => {
                     try {
                         const events = await client.getEventsByThingId(thing._id);
-                        const eventDeletePromises = [];
-                        for (const event of events.value) {
-                            eventDeletePromises.push(client.deleteEvent(event._id));
-                        }
+                        const eventDeletePromises = events.value.map((event) => client.deleteEvent(event._id));
                         await Promise.all(eventDeletePromises);
                         await client.deleteThing(thing._id);
                         resolve();
                     } catch (error) {
                         reject(error);
-                    } 
-                });
-            });
-            await Promise.all(thingDeletePromises)
+                    }
+                }));
+            await Promise.all(thingDeletePromises);
             await client.deleteThingType(thingType.Name, thingTypeResponse.headers.etag);
         }
 
@@ -38,11 +31,11 @@ async function deletePackageCascading(client, packageName) {
         const psts = pstsResponse.d.results;
         psts.sort((a, b) => ((a.DataCategory === 'ReferencePropertyData') ? -1 : ((b.DataCategory === 'ReferencePropertyData') ? 1 : 0)));
         for (const pst of psts) {
-            const pstResponse = await client.getPropertySetType(pst.Name, {}, {resolveWithFullResponse: true});
+            const pstResponse = await client.getPropertySetType(pst.Name, {}, { resolveWithFullResponse: true });
             await client.deletePropertySetType(pst.Name, pstResponse.headers.etag);
         }
-        packageResponse = await client.getPackage(packageName, {resolveWithFullResponse: true});
-    }catch (error) {
+        packageResponse = await client.getPackage(packageName, { resolveWithFullResponse: true });
+    } catch (error) {
         throw (error);
     }
     return client.deletePackage(packageName, packageResponse.headers.etag);
