6c03889ac88a73265126b9c16f8b55987d66ff89
Robert Jakobs <robert.jakobs@sap.com>
2021-07-28 11:43:55 +0200

    Rename client in tests

--- test/integration/step_4_update.test.js
+++ test/integration/step_4_update.test.js
@@ -1,12 +1,11 @@
 const LeonardoIoT = require('../../lib/LeonardoIoT');
 
 describe('4) UPDATE', function () {
-  let iotClient;
+  let client;
 
   // eslint-disable-next-line mocha/no-hooks-for-single-case
   before(function () {
-    // eslint-disable-next-line no-unused-vars
-    iotClient = new LeonardoIoT();
+    client = new LeonardoIoT();
   });
 
   it('none', function () { });
