391d520ee9bee2a538e759f86ae98b2bf32c6929
Robert Jakobs <robert.jakobs@sap.com>
2021-07-28 11:44:59 +0200

    Eslint fix for client in tests

--- test/integration/step_4_update.test.js
+++ test/integration/step_4_update.test.js
@@ -5,6 +5,7 @@ describe('4) UPDATE', function () {
 
   // eslint-disable-next-line mocha/no-hooks-for-single-case
   before(function () {
+    // eslint-disable-next-line no-unused-vars
     client = new LeonardoIoT();
   });
 
