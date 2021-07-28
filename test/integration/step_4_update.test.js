21711f250871f9c55416a8c56349aa9f6903bdd9
Robert Jakobs <robert.jakobs@sap.com>
2021-07-28 11:46:29 +0200

    Delete client variable for update test

--- test/integration/step_4_update.test.js
+++ test/integration/step_4_update.test.js
@@ -1,12 +1,9 @@
 const LeonardoIoT = require('../../lib/LeonardoIoT');
 
 describe('4) UPDATE', function () {
-  let client;
-
   // eslint-disable-next-line mocha/no-hooks-for-single-case
   before(function () {
-    // eslint-disable-next-line no-unused-vars
-    client = new LeonardoIoT();
+    LeonardoIoT();
   });
 
   it('none', function () { });
