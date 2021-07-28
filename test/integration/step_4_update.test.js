d25d4d2bfe55774394096225200cb6b503b3ec2f
Robert Jakobs <robert.jakobs@sap.com>
2021-07-28 11:59:04 +0200

    Fix update integration test

--- test/integration/step_4_update.test.js
+++ test/integration/step_4_update.test.js
@@ -3,7 +3,8 @@ const LeonardoIoT = require('../../lib/LeonardoIoT');
 describe('4) UPDATE', function () {
   // eslint-disable-next-line mocha/no-hooks-for-single-case
   before(function () {
-    LeonardoIoT();
+    // eslint-disable-next-line no-new
+    new LeonardoIoT();
   });
 
   it('none', function () { });
